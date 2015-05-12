bdb0da03a3834deee7c8eadeeda7930cc5e1fdb2
Kevin McGillivray <kmcgillivray@insightcreative.com>
2015-05-12 16:52:26 -0500

    Add firebase.

--- fun.js
+++ fun.js
@@ -1,13 +1,12 @@
-var app = angular.module("occupied", []);
+var app = angular.module("occupied", ["firebase"]);
 
-app.controller('light', ['$scope', function($scope) {
-  $scope.occupied = "false";
-  $scope.status = "No";
-  console.log("test controller");
+app.controller('light', ['$scope', '$firebaseObject', function($scope, $firebaseObject) {
+  var ref = new Firebase("https://insight-occupied.firebaseio.com/occupied");
+  var syncObject = $firebaseObject(ref);
+  syncObject.$bindTo($scope, "occupied");
 
   $scope.changeStatus = function() {
-    $scope.occupied = "true";
-    $scope.status = "Yes";
+    $scope.occupied.$value = $scope.occupied.$value === false ? true: false;
   };
 
 }]);
